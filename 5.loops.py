e79f053b41fb0d1d33c47c13071281f88a53429c
Aysen23 <borisov_aisen@mail.ru>
2021-10-06 16:01:21 +0900

    добавил "генератор словарей"

--- 5.loops.py
+++ 5.loops.py
@@ -98,3 +98,34 @@ my_list = [num + 100 for num in range(10)]
 my_list = [num  for num in range(10) if num % 2 == 0]
 
 # print(my_list)
+
+# *** генератор словарей ***
+
+# ключ значения
+
+# a = {word: len(word) for word in ['hello', 'hi', 'www']}
+# print(a) 
+
+# data = {
+#     'Джефф Безос': "177",
+#     "ИлоН МаСк": "126",
+#     "бернар АрнО": "150",
+#     "БиЛл ГейтС": "124",
+# }
+# new_data = {key.title():int(value)for key, value in data.items()}
+# то же самое без генератора словарей
+# new_data = {} 
+# for key, value in data.items():
+#     new_data[key.title()] = int(value)
+# print(data)
+# print(new_data)
+
+users = [
+    [0, "Bob", "password"],
+    [1, "code", "python"],
+    [2, "Stack", "overflow"],
+    [3, "username", "1234"],
+    [51, "qwerty", "1234"],
+]
+new_users = {user[0]: user for user in users}
+print(new_users[51])

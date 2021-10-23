f3c246843bb5e34953e45c54132ff51c4cd308e0
Aysen23 <borisov_aisen@mail.ru>
2021-10-23 17:57:28 +0900

    added "соль"

--- 10.pwd_gen.py
+++ 10.pwd_gen.py
@@ -8,17 +8,19 @@ from tkinter import Tk, Label, Entry, Button, StringVar
 
 def generator():
     # чтение строки сырой пароли
-    pwd_str = pwd.get()
+    pwd_str = pwd.get() + salt.get()
     # кодирование в байт-строка
     byte_str = pwd_str.encode()
     # хеширование
     hash_str = hashlib.sha256(byte_str)
     # преобразование хеш-строки в обычную строку
-    hex_str = hash_str.hexdigest()[:10]
+    hex_str = hash_str.hexdigest()
+    s = list(hex_str)
+    start = s[:10]  
 
     # вывод хеш-строки
     # print(hex_str)
-    hash_pwd.set(hex_str)
+    hash_pwd.set(start)
 # generator("qwerty")
 
 # объект окна
@@ -27,25 +29,30 @@ window.title("Pwd generator v.0.1")
 
 # объекты для хранения строк
 pwd = StringVar()
+salt = StringVar()
 hash_pwd = StringVar()
 
 # тестирование StringVar
-# pwd.set("qwerty")
+#  pwd.set("qwerty")
 
-# print(pwd.get())
+#  print(pwd.get())
 
 # виджет (компонент) текстовой метки
 lbl = Label(text="Пароль:")
 lbl.grid(row=0, column=0)
 
+lbl_1 = Label(text="Соль:")
+lbl_1.grid(row=1, column=0)
+
 # виджет поля ввода
-Entry(textvariable=pwd).grid(row=0, column=1)
+Entry(textvariable=pwd, show="*").grid(row=0, column=1)
+Entry(textvariable=salt).grid(row=1, column=1)
 
 # виджет кнопки
-Button(text="СТАРТ", command=generator).grid(row=1, column=0)
+Button(text="СТАРТ", command=generator).grid(row=2, column=0)
 
 # виджет поля вывода
-Entry(textvariable=hash_pwd).grid(row=1, column=1)
+Entry(textvariable=hash_pwd).grid(row=2, column=1)
 
 # точка входа программы (место, где программа запускается)
 window.mainloop()
